4c868bce9a149a02fa5f374aef28dc62af64bb48
abidk <Abid>
2013-08-16 20:29:24 +0100

    Refactored code when scanning A+B items.

--- src/main/java/Checkout.java
+++ src/main/java/Checkout.java
@@ -2,13 +2,15 @@ public class Checkout {
 
   private int total = 0;
 
-  public void scan(String item) {
-    if (item.equals("A")) {
-      total = 50;
-    } else if (item.equals("B")) {
-      total = 30;
-    } else if (item.equals("AB")) {
-      total = 80;
+  public void scan(String products) {
+    String[] items = products.split("");
+
+    for (String item : items) {
+      if (item.equals("A")) {
+        total += 50;
+      } else if (item.equals("B")) {
+        total += 30;
+      }
     }
   }
 
