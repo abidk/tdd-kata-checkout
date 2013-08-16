694d696e6644a1fa3d31effd6a98517c2a5f56a7
abidk <Abid>
2013-08-16 21:24:40 +0100

    Refactored code

--- src/main/java/Checkout.java
+++ src/main/java/Checkout.java
@@ -1,26 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Checkout {
 
+  private Map<String, Integer> itemCounter = new HashMap<String, Integer>();
   private int total = 0;
-  private int countA = 0;
 
   public void scan(String products) {
     String[] items = products.split("");
 
     for (String item : items) {
       total += getItemPrice(item);
-      total -= getDiscountPrice(item);
+
+      int itemCount = itemCounter.get(item) != null ? itemCounter.get(item) : 0;
+      itemCounter.put(item, ++itemCount);
     }
+
+    total -= applyDiscounts();
   }
 
-  private int getDiscountPrice(String item) {
-    if (item.equals("A")) {
-      countA++;
-    
-      if (countA > 0 && countA % 3 == 0) {
-        return 20;
+  private int applyDiscounts() {
+    int totalDiscount = 0;
+
+    for (Map.Entry<String, Integer> itemCount : itemCounter.entrySet()) {
+      String item = itemCount.getKey();
+      int count = itemCount.getValue();
+      totalDiscount += getDiscountPrice(item, count);
+    }
+
+    return totalDiscount;
+  }
+
+  private int getDiscountPrice(String item, int count) {
+    int bundleDiscount = 0;
+    for (int x = 0; x <= count; x++) {
+      if (item.equals("A")) {
+        if (x > 0 && x % 3 == 0) {
+          bundleDiscount += 20;
+        }
       }
     }
-    return 0;
+    return bundleDiscount;
   }
 
   private int getItemPrice(String item) {
